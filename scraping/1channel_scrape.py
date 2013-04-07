8da9ba5722b1c64b9ac8797eb226ad04cdaff116
Jeff F <fontas.j+git@gmail.com>
2013-04-07 11:38:05 -0400

    cleaned up 1channel scrape

--- scraping/1channel_scrape.py
+++ scraping/1channel_scrape.py
@@ -1,17 +1,22 @@
 import ch1, imdb, json
-import inspect, pprint, time, pickle, collections
+import inspect, pprint, time, pickle
 
 ## prints out the mov_obj for debugging purposes using prettyprint.
 def pp():
     pprint.pprint((mov_obj))
+
+# open mov_obj in its current state
 f = open('mov_obj.pickle', 'r')
 
 ### The intouchables is not on 1channel
 ### Bicycle thieves not on 1channel
 ### For a few dollars more not on 1channel
 
+
+# begin and end variables--use to control particular movies being scrapted
 begin = 247
 end = 250
+
 # make empty string if no proxy desired
 proxy = "127.0.0.1:9050"
 
@@ -24,31 +29,14 @@ def rw_pickle(f):
     f = open('mov_obj.pickle', 'w')
     pickle.dump(mov_obj, f)
     f.close()
+
 # get the top 250 movies from the imdb list, add them to the movies object, uncomment line to build obj
 # imdb.get_top250(mov_obj)
 
-# create an ordered dictionary ranking movies by ... rank.  Obviously.
-# a = collections.OrderedDict(sorted(mov_obj.items(), key=lambda (k,v): v['IMDB']['Rank']))
-# for x in a
+
+
 # get 1channel details
-# log = open('log.txt', 'w+')
-counter = begin
-# for mov in mov_obj[begin:end]:
-#     # get 1channel info
-#     print "Attempting movie", str(counter) + ":", str(mov['title']) + "........"
-    
-#     ch1.get_1ch_details(mov['title'], mov, proxy)
-# #     # print "OK!"
-# #     # log.write(m)
-#     counter += 1
-#     time.sleep(10)
-
-
-
-
-
-#ch1.get_search_string()
-
+# counter = begin
 # # for mov in mov_obj[(begin-1):end]:
 #     print "Attempting movie", str(counter) + ":", str(mov['title'].encode('ascii','ignore')) + "........"
     
@@ -56,12 +44,5 @@ counter = begin
 #     counter += 1
 #     time.sleep(10)
 #     rw_pickle(f)
-
-
-
-
-
-
-
 
 # f.close()
